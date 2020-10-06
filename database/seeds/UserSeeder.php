5f8ebfcb8c6c627ac9c49a464b4970569e6f3b22
Wahyu Winaldi <wahyuwinaldi@gmail.com>
2020-10-06 21:52:48 +0700

    initial commit

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = User::create([
+            'name' => 'Admin Role',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('12345678'),
+        ]);
+
+        $admin->assignRole('admin');
+
+        $user = User::create([
+            'name' => 'User Role',
+            'email' => 'user@gmail.com',
+            'password' => bcrypt('12345678'),
+        ]);
+
+        $user->assignRole('user');
+    }
+}
